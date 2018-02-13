bec923e1a3783824745545c6ac309405fae16bf9
judisdaid <jmorrissey@saic.edu>
2018-02-13 07:47:54 -0600

    updating p5 compass

--- p5/compass/sketch.js
+++ p5/compass/sketch.js
@@ -9,7 +9,7 @@ function draw() {
   //rotateY(radians(rotationY));
   fill(50);
   textSize(100)
-  textAlign(CENTER)
+  //textAlign(CENTER)
    text(100,800,"test")
  text(200,600,radians(rotationZ))
 }
