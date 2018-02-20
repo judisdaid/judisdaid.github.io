ea8f85c51435a95e5e0cf00bc9274c090d34a4fd
judisdaid <jmorrissey@saic.edu>
2018-02-20 19:47:12 +0200

    changed text

--- 01compass_empty.js
+++ 01compass_empty.js
@@ -10,19 +10,19 @@ oriented = function(e) {
 
 
         if(compassHeading > 315 || compassHeading < 45) {
-                text_area.textContent = "north 4";
+                text_area.textContent = "north 5";
         }
 
         if(compassHeading >= 45 && compassHeading < 135) {
-                text_area.textContent = "east 4";
+                text_area.textContent = "east 5";
         }
 
         if(compassHeading >= 135 && compassHeading < 225) {
-                text_area.textContent = "south 4";
+                text_area.textContent = "south 5";
         }
 
         if(compassHeading >= 225 && compassHeading < 315) {
-                text_area.textContent = "west 4";
+                text_area.textContent = "west 5";
         }
 
 
